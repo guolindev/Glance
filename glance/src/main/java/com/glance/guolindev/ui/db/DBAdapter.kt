da7660ca45e91957e299c06fa23132aee2f457cd
guolindev <sinyu890807@163.com>
2020-08-27 06:50:06 +0800

    Starting implement views for DBActivity.

---
+++ glance/src/main/java/com/glance/guolindev/ui/db/DBAdapter.kt
@@ -0,0 +1,31 @@
+package com.glance.guolindev.ui.db
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *
+ * @author guolin
+ * @since 2020/8/26
+ */
+
+class DBAdapter : RecyclerView.Adapter<DBAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}

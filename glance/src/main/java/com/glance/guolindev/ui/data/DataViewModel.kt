734328c8945b30a58e2d21c3161f0ece92ca5888
guolindev <sinyu890807@163.com>
2020-09-13 07:26:30 +0800

    Add open source declaration.

--- glance/src/main/java/com/glance/guolindev/ui/data/DataViewModel.kt
+++ glance/src/main/java/com/glance/guolindev/ui/data/DataViewModel.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C)  guolin, Glance Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.glance.guolindev.ui.data
 
 import androidx.lifecycle.LiveData
@@ -30,7 +46,7 @@ class DataViewModel(private val repository: DatabaseRepository) : ViewModel() {
         _columnsLiveData.value = Resource.error(throwable.message ?: "Uncaught exception happens")
     }
 
-    fun getColumnsInTable(table: String) = viewModelScope.launch {
+    fun getColumnsInTable(table: String) = viewModelScope.launch(handler) {
         val columns = repository.getColumnsInTable(table)
         _columnsLiveData.value = Resource.success(columns)
     }

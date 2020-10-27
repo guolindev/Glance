b7282c12e607be3b52cdadbe7fe3cd0816684f5c
guolindev <sinyu890807@qq.com>
2020-10-27 17:01:12 +0800

    Assign DBActivity with different and unique taskAffinity.

--- app/src/main/java/com/glance/guolindev/sample/MainActivity.kt
+++ app/src/main/java/com/glance/guolindev/sample/MainActivity.kt
@@ -16,12 +16,19 @@
 
 package com.glance.guolindev.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.glance.guolindev.ui.db.DBActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startGlance.setOnClickListener {
+            val intent = Intent(this, DBActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
